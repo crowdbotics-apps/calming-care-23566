f02e00876897d902d5ec459c8c50bd4158cbe483
Crowdbotics <team@crowdbotics.com>
2020-12-27 19:58:59 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/calming_care_23566/MainActivity.java
@@ -0,0 +1,15 @@
+package com.calming_care_23566;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "calming_care_23566";
+    }
+}
---
+++ ios/calming_care_23566Tests/calming_care_23566Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface calming_care_23566 : XCTestCase
+
+@end
+
+@implementation calming_care_23566
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
